6e1c3c4aa0a113bc4cddc5c3e1bedc923b0264b9
andy <andy@thescrapheap.org>
2014-12-01 15:45:28 +0000

    Added getBearing and getMagnitude to Point

--- Point.java
+++ Point.java
@@ -1,5 +1,7 @@
 package jpml;
 
+import robocode.util.Utils;
+
 /**
  * Created by abs1g14 on 01/12/14.
  */
@@ -10,7 +12,19 @@ public class Point {
         this.y = y;
     }
 
+    public double distance(Point point) {
+        return distance(point.x,point.y);
+    }
+
     public double distance(double x, double y) {
         return Math.sqrt((x*x)+(y*y));
+    }
+
+    public double getBearing() {
+        return Utils.normalAbsoluteAngle(Math.atan2(this.x - x, this.y - y));
+    }
+
+    public double getMagnitude() {
+        return distance(0,0);
     }
 }
