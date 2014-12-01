202b814e63d35c0975082e7810d8c94cc84d956b
andy <andy@thescrapheap.org>
2014-12-01 11:46:45 +0000

    Fixed radar syntax errors

--- Radar.java
+++ Radar.java
@@ -1,17 +1,19 @@
 package jpml;
 
+import robocode.ScannedRobotEvent;
+
 /**
  * MyClass - a class by (your name here)
  */
 public class Radar
 {
 	private Knowledge k;
-	
+
 	public Radar(Knowledge k){
 		this.k = k;
 	}
 	 
-	public onScannedRobot(ScannedRobotEvent e){
+	public void onScannedRobot(ScannedRobotEvent e){
 		k.updateRobot(e.getName(), e.getBearingRadians(), e.getDistance());
 	}
 	
